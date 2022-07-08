eaa1398b9647b9e8617ecf700731be06e44b6d34
abdalrzag eisa <bmjfdrh@gmail.com>
2022-07-08 04:49:23 +0300

    read files

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,29 @@ mod args;
 use std::{path::Path, panic};
 use args::RcatArgs;
 use clap::Parser;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let cli = RcatArgs::parse();
 
-    
-}
+    check_paths(&cli.paths);
 
-fn read_file(path: &String) -> () {}
+    for path in cli.paths.iter() {
+        let stdout = std::io::stdout();
+        let mut handle = stdout.lock();
+
+        match File::open(path).and_then(|mut file| std::io::copy(&mut file, &mut handle)) {
+            Err(err) => eprintln!("{}", err),
+            _ => (),
+        }
+    }
+}
 
 /// Check if file exists
 fn file_exists(path: &String) -> bool {
-    if ! Path::new(path).exists() {
-        panic!("'{}': No such file", path)
+    if !Path::new(path).exists() {
+        panic!("'{}': No such file or directory", path)
     }
 
     true
@@ -23,9 +33,18 @@ fn file_exists(path: &String) -> bool {
 
 /// Check if path is path of a file
 fn is_a_file(path: &String) -> bool {
-    if Path::new(path).is_file() {
+    if ! Path::new(path).is_file() {
         panic!("'{}': Is a directory", path)
     }
 
     true
+}
+
+/// Check if paths is not a file or doesn't exists
+/// before reading them.
+fn check_paths(paths: &Vec<String>) -> () {
+    for path in paths.iter() {
+        file_exists(&path);
+        is_a_file(&path);
+    }
 }
