b3e7bf23c3ff2d6b4159c24ce0283d31b39ad691
abdalrzag eisa <bmjfdrh@gmail.com>
2022-07-08 04:11:32 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod args;
+
+use std::{path::Path, panic};
+use args::RcatArgs;
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = RcatArgs::parse();
+
+    
+}
+
+fn read_file(path: &String) -> () {}
+
+/// Check if file exists
+fn file_exists(path: &String) -> bool {
+    if ! Path::new(path).exists() {
+        panic!("'{}': No such file", path)
+    }
+
+    true
+}
+
+/// Check if path is path of a file
+fn is_a_file(path: &String) -> bool {
+    if Path::new(path).is_file() {
+        panic!("'{}': Is a directory", path)
+    }
+
+    true
 }
