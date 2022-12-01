40870c5a7838c6f51f9e92b3e82e76668b5bfe7f
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-01 08:41:40 +0300

    add new options

--- src/args.rs
+++ src/args.rs
@@ -13,4 +13,21 @@ pub struct Args {
     /// Ignore path if not found or is a directory
     #[clap(short, long)]
     pub skip: bool,
+
+    /// Print filename before printing contents
+    #[clap(short, long)]
+    pub print_filename: bool,
+
+    /// Print filename as absolute path
+    #[clap(short, long)]
+    pub print_absolute_path: bool,
+
+    /// Add a separater between files
+    #[clap(short, long)]
+    pub sep: bool,
+
+    /// Separater to use between files
+    #[clap(short, long)]
+    pub sep_v: Option<String>,
+
 }
