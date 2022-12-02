44d49ca29c74bd85ca0ad6cd9e741662359ac658
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-02 05:56:22 +0300

    make main.rs smaller

---
+++ src/cat_file.rs
@@ -0,0 +1,61 @@
+use crate::args::Args;
+use colored::Colorize;
+
+pub fn cat_file(filename: &String, contents: String, cli: &Args) -> () {
+    if is_atty() {
+        if cli.sep {
+            println!("{}", cli.sep_v);
+        }
+
+        if cli.print_filename {
+            if cli.absolute_path {
+                let pathbuf = std::path::PathBuf::from(filename);
+                let abs_path = std::fs::canonicalize(&pathbuf).unwrap();
+
+                println!("file: {}", abs_path.to_str().unwrap());
+            } else {
+                println!("file: {}", filename);
+            }
+        }
+    }
+
+    // Only display line numbers if printing to tty
+    if cli.numbers && is_atty() {
+        let max_lines_number: usize = contents.lines().count();
+
+        for (index, line) in contents.lines().enumerate() {
+            let line_number = index + 1;
+
+            println!(
+                "{line_number} {line}",
+                line_number = pad_line_number(&line_number, &max_lines_number),
+                line = line
+            );
+        }
+    } else {
+        println!("{}", contents);
+    }
+}
+
+fn pad_line_number(number: &usize, max_number: &usize) -> String {
+    let number_length = get_number_length(number);
+    let max_number_length = get_number_length(max_number);
+
+    format!(
+        "{}{}",
+        " ".repeat(max_number_length - number_length),
+        number.to_string().black()
+    )
+}
+
+fn get_number_length(n: &usize) -> usize {
+    n.to_string().chars().count()
+}
+
+fn is_atty() -> bool {
+    if atty::isnt(atty::Stream::Stdout) {
+        return false;
+    }
+
+    return true;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod args;
+mod cat_file;
 
 use args::Args;
+use cat_file::cat_file;
 use clap::Parser;
-use colored::Colorize;
 use std::{fs::File, io::Read};
 
 fn main() {
@@ -35,61 +36,7 @@ fn main() {
             _ => {}
         };
 
-        if is_atty() {
-            if cli.sep {
-                println!("{}", cli.sep_v);
-            }
-
-            if cli.print_filename {
-                if cli.absolute_path {
-                    let pathbuf = std::path::PathBuf::from(path);
-                    let abs_path = std::fs::canonicalize(&pathbuf).unwrap();
-
-                    println!("file: {}", abs_path.to_str().unwrap());
-                } else {
-                    println!("file: {}", path);
-                }
-            }
-        }
-
-        // Only display line numbers if printing to tty
-        if cli.numbers && is_atty() {
-            let max_lines_number: usize = contents.lines().count();
-
-            for (index, line) in contents.lines().enumerate() {
-                let line_number = index + 1;
-
-                println!(
-                    "{line_number} {line}",
-                    line_number = pad_line_number(&line_number, &max_lines_number),
-                    line = line
-                );
-            }
-        } else {
-            println!("{}", contents);
-        }
+        cat_file(path, contents, &cli);
     }
 }
 
-fn pad_line_number(number: &usize, max_number: &usize) -> String {
-    let number_length = get_number_length(number);
-    let max_number_length = get_number_length(max_number);
-
-    format!(
-        "{}{}",
-        " ".repeat(max_number_length - number_length),
-        number.to_string().black()
-    )
-}
-
-fn get_number_length(n: &usize) -> usize {
-    n.to_string().chars().count()
-}
-
-fn is_atty() -> bool {
-    if atty::isnt(atty::Stream::Stdout) {
-        return false;
-    }
-
-    return true;
-}
